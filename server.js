3881efa37f447c826c44536af6e074757cebe306
Sathya Narayanan <sathya.n@beaconstac.com>
2023-05-03 02:08:32 +0530

    Implemented sheets api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import pages from "./api/pages/pages.route.js"
 import buttons from "./api/buttons/buttons.route.js"
 import analytics from "./api/analytics/analytics.route.js"
+import sheets from "./api/sheets/sheets.route.js"
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/api/v1/pages", pages)
 app.use("/api/v1/buttons", buttons)
 app.use("/api/v1/analytics", analytics)
+app.use("/api/v1/sheets", sheets)
 app.use("*", (req, res) => res.status(404).json({detail: "Not found"}))
 
 export default app
